f24e71768e5cde3616fd0093bd4c49303bba6461
Lennart Hengstmengel <lennart@farenji.net>
2023-07-24 20:39:45 +0200

    fix ret.type

--- src/Interfaces/ResultInterface.php
+++ src/Interfaces/ResultInterface.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 namespace Vazaha\Mastodon\Interfaces;
 
-use Illuminate\Support\Collection;
 use Vazaha\Mastodon\Interfaces\ModelInterface;
 
 interface ResultInterface
 {
     /**
-     * @return null|array<int, \Vazaha\Mastodon\Interfaces\ModelInterface>
+     * @return array<int, \Vazaha\Mastodon\Interfaces\ModelInterface>
      */
-    public function getModels(): ?array;
+    public function getModels(): array;
 
     public function getModel(): ?ModelInterface;
 
