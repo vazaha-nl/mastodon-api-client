c9d6f169fb9ef519f6f8b99798d38ac4d3da7387
Lennart Hengstmengel <lennart@farenji.net>
2023-07-31 18:00:23 +0200

    removed unneeded symfony deps and code

--- config/bundles.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-];
