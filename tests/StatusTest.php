d4b0c97a9019ad2fc91018ec849d702c9e60c67c
Lennart Hengstmengel <lennart@farenji.net>
2023-07-31 02:51:31 +0200

    make tests in sync with the new reality

--- tests/StatusTest.php
+++ tests/StatusTest.php
@@ -7,11 +7,10 @@ namespace Tests;
 use DateTimeInterface;
 use PHPUnit\Framework\TestCase;
 use Tests\Concerns\CreatesMockClient;
-use Vazaha\Mastodon\Collections\StatusTagCollection;
+use Vazaha\Mastodon\Collections\Status\TagCollection;
 use Vazaha\Mastodon\Models\AccountModel;
-use Vazaha\Mastodon\Models\ApplicationModel;
+use Vazaha\Mastodon\Models\Status\TagModel;
 use Vazaha\Mastodon\Models\StatusModel;
-use Vazaha\Mastodon\Models\StatusTagModel;
 use Vazaha\Mastodon\Requests\GetStatusRequest;
 use Vazaha\Mastodon\Results\StatusResult;
 
@@ -35,10 +34,9 @@ class StatusTest extends TestCase
         self::assertEquals('103270115826048975', $status->id);
         self::assertInstanceOf(AccountModel::class, $status->account);
         self::assertEquals('1', $status->account->id);
-        self::assertInstanceOf(ApplicationModel::class, $status->application);
         self::assertInstanceOf(DateTimeInterface::class, $status->created_at);
-        self::assertInstanceOf(StatusTagCollection::class, $status->tags);
-        self::assertInstanceOf(StatusTagModel::class, $status->tags->first());
+        self::assertInstanceOf(TagCollection::class, $status->tags);
+        self::assertInstanceOf(TagModel::class, $status->tags->first());
         self::assertEquals('tagname1', $status->tags->first()->name);
     }
 }
